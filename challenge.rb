311f1a065a1aecfa707967b6a13ec3e8de9e4603
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-07-25 18:36:26 +0400

    Resolve linter errors

--- challenge.rb
+++ challenge.rb
@@ -1,17 +1,15 @@
-def decode_char(morse_code) = {
+def decode_char(morse_code)
   morse_code_map = {
-    ".-"   => "A", "-..." => "B", "-.-." => "C", "-.."  => "D", "."    => "E",
-    "..-." => "F", "--."  => "G", "...." => "H", ".."   => "I", ".---" => "J",
-    "-.-"  => "K", ".-.." => "L", "--"   => "M", "-."   => "N", "---"  => "O",
-    ".--." => "P", "--.-" => "Q", ".-."  => "R", "..."  => "S", "-"    => "T",
-    "..-"  => "U", "...-" => "V", ".--"  => "W", "-..-" => "X", "-.--" => "Y",
-    "--.." => "Z"
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z'
   }
-  morse_code_map[morse_code] || ""
+  morse_code_map[morse_code] || ''
 end
-}
 
 def decode_word(morse_word)
-  decode_word = morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join
-  decode_word
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
 end

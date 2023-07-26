c6a04fd75410fdd2293874171c12d9075d21a0a4
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-07-26 11:17:39 +0100

    modified the challenge file

--- challenge.rb
+++ challenge.rb
@@ -7,9 +7,20 @@ def decode_char(morse_code)
     '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
     '--..': 'Z'
   }
-  morse_code_map[morse_code] || ''
+  morse_code_map[morse_code.to_sym] || ''
 end
 
 def decode_word(morse_word)
   morse_word.split.map { |morse_char| decode_char(morse_char) }.join
 end
+
+def decode(message)
+  message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+char = decode_char('.-')
+puts char
+word = decode_word('-- -.--')
+puts word
+message = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts message
